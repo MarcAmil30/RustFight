9d1fddf2c9c3b359e81273d9f3600e9135fdb9e2
MarcAmil30 <marc.amil18@imperial.ac.uk>
2022-06-19 16:05:48 +0100

    init

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import os 
 
+
 class Game:
     def __init__(self):
         self.width = 100
@@ -11,6 +12,7 @@ class Game:
         self.lives = 10
         self.money = 100
         self.bg = pygame.image.load(os.path.join("game_assets", "bg.png"))
+        self.clicks = []
 
     
     def run(self):
@@ -21,13 +23,25 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False 
+            
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.clicks.append(pos)
+                    print(pos)
+
+            self.draw()
 
         pygame.quit()
     
     def draw(self):
         self.win.blit(self.bg, (0,0))
+        for p in self.clicks:
+            pygame.draw.circle(self.win, (255,0,0), p([0], p[1]), 5, 0)
         pygame.display.update()
-    
+
+g =Game()
+g.run
 
 
 
