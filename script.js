18b18bdcb121006cf2359d8541583d9f8f313c58
MarcAmil30 <marc.amil18@imperial.ac.uk>
2022-06-20 11:52:23 +0100

    Added enemies, gridlines and collision

--- script.js
+++ script.js
@@ -8,11 +8,18 @@ canvas.height = 600;
 const cellSize = 100; 
 const cellGap = 3; 
 const gameGrid = [];
+const defenders = [];
+let numberOfResources = 300; 
+const enemies = [];
+const enemyPositions = [];
+let enemiesInterval = 600;
+let frame = 0; 
+let gameOver = false; 
 
 // mouse 
 const mouse = {
-    x: undefined,
-    y: undefined,
+    x: 10,
+    y: 10,
     width: 0.1, 
     height: 0.1, 
 }
@@ -41,8 +48,11 @@ class Cell {
         this.height = cellSize; 
     }
     draw(){
-        ctx.strokeStyle = 'black';
-        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        if (mouse.x && mouse.y && collision(this, mouse)){
+            ctx.strokeStyle = 'black';
+            ctx.strokeRect(this.x, this.y, this.width, this.height);
+        }
+
     }
 }
 
@@ -64,15 +74,129 @@ function handleGameGrid(){
 
 
 // projectiles
+
+
+
 // defenders
+class Defender { 
+    constructor(x, y){
+        this.x = x; 
+        this.y = y; 
+        this.width = cellSize; 
+        this.height = cellSize; 
+        this.shooting = false;
+        this.health = 100; 
+        this.projectiles = [];
+        this.timer = 0;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'gold';
+        ctx.font = '20px VT323';
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25);
+    }
+}
+
+canvas.addEventListener('click', function(){
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    if (gridPositionY < cellSize) return;
+    for (let i = 0; i < defenders.length; i++){
+        if (defenders[i].x === gridPositionX && defenders[i].y === gridPositionY)
+        return;
+    }
+    let defenderCost = 100; 
+    if (numberOfResources >= defenderCost){
+        defenders.push(new Defender(gridPositionX, gridPositionY));
+        numberOfResources -= defenderCost; 
+    }
+});
+
+function handleDefenders(){
+    for (let i = 0; i < defenders.length; i++){
+        defenders[i].draw();
+        for (let j = 0; j  < enemies.length; j++){
+            if (collision(defenders[i], enemies[j])){
+                enemies[j].movement = 0;
+                defenders[i].health -= 0.2;
+            }
+            if (defenders[i].health <= 0){
+                defenders.splice(i, 1);
+                i--;
+                enemies[j].movement = enemies[j].speed;
+            }
+        }
+    }
+} 
+
 // enemies
+class Enemy {
+    constructor(verticalPosition){
+        this.x = canvas.width;
+        this.y = verticalPosition;
+        this.width = cellSize;
+        this.height = cellSize; 
+        this.speed = Math.random() * 0.2 + 0.4; 
+        this.movement = this.speed;
+        this.health = 100;
+        this.maxHealth = this.health;
+    }
+
+    update(){
+        this.x -= this.movement; 
+    }
+    draw(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'black';
+        ctx.font = '20px VT323';
+        ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 25);
+
+    }
+}
+
+function handleEnemies(){
+    for (let i = 0; i < enemies.length; i++){
+        enemies[i].update();
+        enemies[i].draw();
+        if (enemies[i].x < 0){
+            gameOver = true;
+        }
+    }
+    if (frame % enemiesInterval === 0){
+        let verticalPosition = Math.floor(Math.random() * 5 + 1) * cellSize; 
+        enemies.push(new Enemy(verticalPosition))
+        enemyPositions.push(verticalPosition);
+        if (enemiesInterval > 120) enemiesInterval -=50; 
+    }
+}
+
+
 // resources 
 // utilities
+function handleGameStatus(){
+    ctx.fillStyle = 'gold';
+    ctx.font = '30px VT323';
+    ctx.fillText('Resources:' + numberOfResources, 20, 55);
+    if (gameOver) {
+        ctx.fillStyle = 'black';
+        ctx.font = '120px VT323';
+        ctx.fillText('GAME OVER', 135, 330)
+    }
+}
+
+
 function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
-    handleGameGrid()
-    requestAnimationFrame(animate);
+    handleGameGrid();
+    handleDefenders();
+    handleEnemies();
+    handleGameStatus();
+    frame++;
+    if (!gameOver)requestAnimationFrame(animate);
 
 }
 animate(); 
